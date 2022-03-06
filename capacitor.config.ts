322deaeb1a5927ac0e0475d30d50a0a8a1471c3b
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2022-03-06 13:31:18 -0500

    Add capacitor and android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wjom.groceries',
+  appName: 'groceries-app',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
